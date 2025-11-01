b0ee4622821f7191d3b96691195a27e37e657633
ForgeSolucoesEstagio <juniorfranca632@gmail.com>
2025-10-31 23:03:14 -0300

    feat: add components at home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,8 @@
+import Footer from "../../components/Footer/Footer"
 import NavBar from "../../components/NavBar/NavBar"
+import About from "./sections/About/About"
 import Hero from "./sections/Hero/Hero"
+import Projects from "./sections/Projects/Project"
 
 const Home = () => {
 
@@ -7,6 +10,9 @@ const Home = () => {
     <>
       <NavBar/>
       <Hero/>
+      <About/>
+      <Projects/>
+      <Footer/>
     </>
   )
 }
