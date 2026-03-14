4f7aefb4fbfbdc4f33eff9862a5a27ce7aaf150b
marcus_jr” <juniorfranca632@gmail.com>
2026-03-14 00:22:30 -0300

    feat: subindo dados de projetos na aplicação

--- src/pages/Home/sections/Projects/Project.tsx
+++ src/pages/Home/sections/Projects/Project.tsx
@@ -11,42 +11,60 @@ const Projects: React.FC = () => {
 
     const projects = [
         {
-            title: "Exemplo de Projeto",
-            subtitle: "Jul 2023 - Dez 2023",
-            srcImg: "/src/assets/images/project-trello.png",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porta semper velit vel rutrum. Aliquam vulputate, nisi eget tristique mattis, nisi sem faucibus eros, a auctor felis sem ut mauris. Phasellus a ultrices elit. Curabitur ut diam eu orci auctor pretium.",
-            technologies: "Tecnologias: JavaScript, HTML, CSS, Canvas Graphics",
-            websiteURL: "https://trello.com/",
-            codeURL: "https://github.com/",
+            title: "Meu Portfólio",
+            subtitle: "Ago 2025 - Ago 2025",
+            srcImg: "/src/assets/images/portfolio.png",
+            description: "Meu portfólio pessoal, contendo projetos e experiências profissionais.",
+            technologies: "Tecnologias: React, TypeScript, Vite, Material UI, HTML, CSS",
+            websiteURL: "https://marcusjrdev.netlify.app/",
+            codeURL: "https://github.com/Marcus-Jr/my-portfolio",
+            date: "2025-08-01",
         },
         {
-            title: "Exemplo de Projeto",
-            subtitle: "Jul 2023 - Dez 2023",
-            srcImg: "/src/assets/images/project-financas.png",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porta semper velit vel rutrum. Aliquam vulputate, nisi eget tristique mattis, nisi sem faucibus eros, a auctor felis sem ut mauris. Phasellus a ultrices elit. Curabitur ut diam eu orci auctor pretium.",
-            technologies: "Tecnologias: JavaScript, HTML, CSS, Canvas Graphics",
-            websiteURL: "https://trello.com/",
-            codeURL: "https://github.com/",
+            title: "Jogo da Cobrinha",
+            subtitle: "Mai 2024 - Mai 2024",
+            srcImg: "/src/assets/images/snake-game-over.png",
+            description: "Jogo divertido e clássico da cobrinha. Você controla a cobra para comer a comida e crescer, mas cuidado para não bater nas paredes ou em si mesmo!",
+            technologies: "Tecnologias: JavaScript, HTML, CSS",
+            websiteURL: " https://bysnakegame.netlify.app/",
+            codeURL: "https://github.com/Marcus-Jr/Snake-Game",
+            date: "2024-05-01",
         },
         {
-            title: "Projeto Craze Maze",
-            subtitle: "Jul 2019 - Mai 2019",
-            srcImg: "/src/assets/images/project1-craze-maze.gif",
-            description: "Jogo para escapar do labirinto — mas não apenas isso. Foi criado um algoritmo que gera aleatoriamente um novo labirinto a cada vez que o jogo é iniciado. Neste jogo, o usuário pode usar as teclas do teclado para se mover até encontrar a bandeira e vencer o jogo.",
-            technologies: "Tecnologias: JavaScript, HTML, CSS, Canvas Graphics",
-            websiteURL: "https://adrianasaty.github.io/ironhack-project1-craze-maze/index.html",
-            codeURL: "https://github.com/AdrianaSaty/ironhack-project1-craze-maze",
+            title: "TaskFlow - Gerenciador de Tarefas",
+            subtitle: "Set 2026 - Set 2026",
+            srcImg: "/src/assets/images/task-flow.png",
+            description: "Aplicação de gerenciamento de tarefas desenvolvida em React, permitindo aos usuários criar, editar e excluir tarefas de forma intuitiva.",
+            technologies: "Tecnologias: Vue.js, JavaScript, HTML, CSS",
+            websiteURL: "https://bytaskflow.netlify.app/",
+            codeURL: "https://github.com/Marcus-Jr/to-do-list-pro",
+            date: "2025-09-01",
         },
         {
-            title: "Projeto Blotting",
-            subtitle: "Jul 2019 - Mai 2019",
-            srcImg: "/src/assets/images/project2-blotting.png",
-            description: "Jogo para escapar do labirinto — mas não apenas isso. Foi criado um algoritmo que gera aleatoriamente um novo labirinto a cada vez que o jogo é iniciado. Neste jogo, o usuário pode usar as teclas do teclado para se mover até encontrar a bandeira e vencer o jogo.",
-            technologies: "Tecnologias: JavaScript, HTML, CSS, Canvas Graphics",
-            websiteURL: "https://adrianasaty.github.io/ironhack-project1-craze-maze/index.html",
-            codeURL: "https://github.com/AdrianaSaty/ironhack-project1-craze-maze",
+            title: "Projeto de ONG",
+            subtitle: "Nov 2025 - Nov 2025",
+            srcImg: "/src/assets/images/ong-outubro-rosa.png",
+            description: "Projeto de landing page construida em React, visando conscientização e prevenção contra o câncer de mama.",
+            technologies: "Tecnologias: React, TypeScript, Vite, HTML, Material UI, CSS",
+            websiteURL: "https://outubrorosaong.netlify.app/",
+            codeURL: "https://github.com/Marcus-Jr/ong-outubro-rosa",
+            date: "2025-11-01",
+        },
+        {
+            title: "Snip.io - Encurtador de URL",
+            subtitle: "Mar 2026 - Mar 2026",
+            srcImg: "/src/assets/images/snip-io.png",
+            description: "Encurtador de URL que transforma links longos em versões compactas para fácil compartilhamento. Armazenamento via cache de servidor utilizando Map em memória persistida no objeto global do Next.js.",
+            technologies: "Tecnologias: React, Next.js, TypeScript, HTML, Tailwind CSS",
+            websiteURL: "https://snip-io.netlify.app/",
+            codeURL: "https://github.com/Marcus-Jr/url-shortner",
+            date: "2026-03-01   ",
         },
     ];
+
+    const projetosOrdenados = [...projects].sort((a,b) => {
+        return new Date(b.date).getTime() - new Date(a.date).getTime();
+    });
 
     return (
         <StyledExperience>
@@ -55,7 +73,7 @@ const Projects: React.FC = () => {
                     <Typography variant="h2" textAlign="center" color="primary.contrastText">Projetos</Typography>
                 </Box>
                 <Grid container spacing={5} pb={3}>
-                    {projects.map((project: ProjectCardProps, index: number) => (
+                    {projetosOrdenados.map((project: ProjectCardProps, index: number) => (
                         <Grid size={{ md: 6 }} key={index}>
                             <AnimatedComponent moveDirection={index % 2 == 0 ? "right" : "left"}>
                                 <ProjectCard
