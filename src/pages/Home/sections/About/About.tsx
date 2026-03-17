57fad0e047dffb4177d00d368c0d936fb4baa6c0
marcus_jr” <juniorfranca632@gmail.com>
2026-03-17 18:38:33 -0300

    style: icons logos

--- src/pages/Home/sections/About/About.tsx
+++ src/pages/Home/sections/About/About.tsx
@@ -2,6 +2,9 @@ import { Box, Card, Container, Grid, Typography, styled } from "@mui/material"
 import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
 import SchoolIcon from '@mui/icons-material/School';
 import AnimatedComponent from "../../../../components/AnimatedComponent/AnimatedComponent";
+import { alpha } from "@mui/material/styles";
+import { Tooltip } from "@mui/material";
+import type { JSX } from "react";
 
 const About: React.FC = () => {
 
@@ -14,8 +17,52 @@ const About: React.FC = () => {
         }
     }));
 
+    const StyledCardLogo = styled(Card)(({ theme }) => ({
+        padding: "10px 10px",
+        textAlign: "center",
+        marginBottom: "10px",
+        borderRadius: "20px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",   
+        width: "110px",
+        height: "110px",
+        boxShadow: `0 0 10px ${alpha(theme.palette.primary.main, 0.2)}`,
+        '&:hover': {
+            backgroundColor: theme.palette.secondary.light
+        }
+    }));
+
+    const StyledLogo = styled("img")({
+        width: "60px",
+        height: "60px",
+        marginBottom: "5px",
+        '&:hover': {
+            transform: "scale(1.2)",
+            transition: "transform 0.3s ease-in-out"
+        }
+    });
+
+    const skillLink = (skill: string) => {
+        const links: Record<string, JSX.Element> = {
+            "PHP": <StyledLogo src="https://logo.svgcdn.com/devicon/php-original.svg" alt="PHP"/>,
+            "Javascript": <StyledLogo src="https://logo.svgcdn.com/devicon/javascript-original.svg" alt="Javascript" />,
+            "Typescript": <StyledLogo src="https://logo.svgcdn.com/devicon/typescript-original.svg" alt="Typescript" />,
+            "React": <StyledLogo src="https://logo.svgcdn.com/devicon/react-original.svg" alt="React" />,
+            "Node.Js": <StyledLogo src="https://logo.svgcdn.com/devicon/nodejs-original.svg" alt="Node.Js" />,
+            "Vue.js": <StyledLogo src="https://logo.svgcdn.com/devicon/vuejs-original.svg" alt="Vue.js" />,
+            "HTML-5": <StyledLogo src="https://logo.svgcdn.com/devicon/html5-original.svg" alt="HTML-5" />,
+            "CSS": <StyledLogo src="https://logo.svgcdn.com/devicon/css3-original.svg" alt="CSS" />,
+            "Quasar": <StyledLogo src="https://logo.svgcdn.com/devicon/quasar-original.svg" alt="Quasar" />,
+            "PostgreSQL": <StyledLogo src="https://logo.svgcdn.com/devicon/postgresql-original.svg" alt="PostgreSQL" />,
+            "Bootstrap": <StyledLogo src="https://logo.svgcdn.com/devicon/bootstrap-original.svg" alt="Bootstrap" />,
+            "Material UI": <StyledLogo src="https://logo.svgcdn.com/devicon/materialui-original.svg" alt="Material UI" />
+        };
+        return links[skill] || <Typography>{skill}</Typography>;
+    }
+
     const skillsSet = [
-        "PHP", "Javascript", "Typescript", "React", "Node.Js", "Vue.js", "HTML", "CSS", "Quasar", "SQL", "Bootstrap", "Material UI"
+        "PHP", "Javascript", "Typescript", "React", "Node.Js", "Vue.js", "HTML-5", "CSS", "Quasar", "PostgreSQL", "Bootstrap", "Material UI"
     ]
 
     return (
@@ -59,9 +106,11 @@ const About: React.FC = () => {
                     <Grid container spacing={3} justifyContent="center">
                         {skillsSet.map((skill, index) => (
                             <Grid key={index} size={{ xs: 5, sm: 4, md: 2, lg: 2 }}>
-                                <StyledCard variant="outlined">
-                                    {skill}
-                                </StyledCard>
+                                <Tooltip title={skill} arrow>
+                                    <StyledCardLogo variant="outlined">
+                                        {skillLink(skill)}
+                                    </StyledCardLogo>
+                                </Tooltip>
                             </Grid>
                         ))}
                     </Grid>
